623c091dc92161729d8c102dd6426326241293f5
Ni <nisargedu2015@gmail.com>
2025-06-15 19:52:31 -0700

    debug run_demo2.py

--- scripts/run_demo2.py
+++ scripts/run_demo2.py
@@ -57,7 +57,7 @@ if __name__=="__main__":
   stereo_params = np.load(args.stereo_params_npz_file, allow_pickle=True)
         
   P1 = stereo_params['P1']
-  P1[:2] *= scale
+  P1[:2] *= args.scale
   f_left = P1[0,0]
   baseline = stereo_params['baseline']
 
