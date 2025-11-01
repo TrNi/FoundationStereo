1376fd738e10f35130731c1399e8f6f3bb0c9c9e
Ni <nisargedu2015@gmail.com>
2025-10-31 19:55:24 -0700

    updated total size

--- scripts/run_demo2.py
+++ scripts/run_demo2.py
@@ -302,8 +302,8 @@ if __name__=="__main__":
 
             disp_chunk = np.concatenate(disp_chunk, axis=0).reshape(N_max,round(H/resize_factor),round(W/resize_factor)).astype(np.float16)
             depth_chunk = np.concatenate(depth_chunk, axis=0).reshape(N_max,round(H/resize_factor),round(W/resize_factor)).astype(np.float16)
-            write_h5_chunk(f'{args.out_dir}/leftview_disp_depth.h5', 'disp', disp_chunk, prev_start_idx, shape=(N_max,round(H/resize_factor),round(W/resize_factor)),dtype=np.float16)
-            write_h5_chunk(f'{args.out_dir}/leftview_disp_depth.h5', 'depth', depth_chunk, prev_start_idx, shape=(N_max,round(H/resize_factor),round(W/resize_factor)),dtype=np.float16)
+            write_h5_chunk(f'{args.out_dir}/leftview_disp_depth.h5', 'disp', disp_chunk, prev_start_idx, shape=(chunk_size*10,round(H/resize_factor),round(W/resize_factor)),dtype=np.float16)
+            write_h5_chunk(f'{args.out_dir}/leftview_disp_depth.h5', 'depth', depth_chunk, prev_start_idx, shape=(chunk_size*10,round(H/resize_factor),round(W/resize_factor)),dtype=np.float16)
             # with h5py.File(f'{args.out_dir}/leftview_disp_depth.h5', 'w') as f:
             #   f.create_dataset('disp', data=disp_chunk, compression='gzip')
             #   f.create_dataset('depth', data=depth_chunk, compression='gzip')
